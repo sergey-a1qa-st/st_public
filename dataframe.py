cb5976825228c33c29fe7d977954e14950217d98
sergey-a1qa-st <97686632+sergey-a1qa-st@users.noreply.github.com>
2022-03-14 14:01:19 -0400

    Create dataframe.py

---
+++ dataframe.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.dataframe((("apple", "banana", "cherry", "apple", "cherry"),
+            ("obladi", "oblada", "life", "goes", "on"),
+            ("who", "let", "the", "dogs", "out"),
+            ("Kids", "!", "Are", "you", "ready")))
